d4c141f558e550fd224c874e1d5f7e88402d65ed
ruihao_glo <ruihao666@888.com>
2019-10-07 13:48:12 +0800

    init repo

---
+++ src/main/java/com/ruihao/spring_boot/SpringBootSzApplication.java
@@ -0,0 +1,13 @@
+package com.ruihao.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruihao/spring_boot/SpringBootSzApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ruihao.spring_boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSzApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
